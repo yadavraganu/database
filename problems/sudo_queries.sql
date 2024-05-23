0e8df82376df8d263345635403c87a17d53db1cb
Anurag Yadav <yadavraganu@gmail.com>
2024-05-23 14:48:57 +0530

    pattern printing

--- problems/sudo_queries.sql
+++ problems/sudo_queries.sql
@@ -1,2 +1,21 @@
 /* Find the date which is 2 weeks after this week's sunday */
 SELECT DATEADD(WEEK,2,DATEADD(DAY,8-DATEPART(WEEKDAY,GETDATE()),GETDATE()));
+
+/* Write sql query to print below pattern
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+***********
+*/
+WITH CTE AS (
+SELECT 1 AS NUM
+UNION ALL
+SELECT NUM + 1 AS NUM FROM CTE WHERE  NUM < 11)
+SELECT REPLICATE('*', NUM) FROM CTE;
